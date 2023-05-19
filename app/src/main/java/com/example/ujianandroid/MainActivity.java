d198661adeef4d4e596e1fcd9efa1946645118fd
Fahrizalap <fahrizalap1@gmail.com>
2023-05-19 14:56:23 +0700

    Menambahkan Perulangan

--- app/src/main/java/com/example/ujianandroid/MainActivity.java
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -32,15 +32,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String isian_nama_depan = edNamaDepan.getText().toString();
                 String isian_nama_belakang = edNamaBelakang.getText().toString();
-                String isian_umur = edUmur.getText().toString();
+
+                int jmlUmur = Integer.parseInt(edUmur.getText().toString());
 
 
                 if(isian_nama_depan.isEmpty()){
                     Toast.makeText(getApplicationContext(), "Isian masih kosong", Toast.LENGTH_SHORT).show();
                 }else{
-                    String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang).concat(isian_umur);
-                    // daftar_nama.clear();
-                    daftar_nama.add(nama_lengkap);
+                    String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang);
+                    daftar_nama.clear();
+                    for (int i = 0; i < jmlUmur; i++) {
+                        daftar_nama.add(nama_lengkap);
+                    }
                     edNamaDepan.setText("");
                     edNamaBelakang.setText("");
                     edUmur.setText("");
